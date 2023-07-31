ed64a19a3b24610a5240533985f686873b62ea36
elifozker <elif_ozker@hotmail.com>
2023-07-31 16:56:36 +0300

    google auth added

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,16 +11,20 @@ export const metadata = {
 const RootLayout = ({ children }) => {
 
   return (
-    
+
     <html lang="eng">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
-        <main className='app'>
-          <Nav/>
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className='app'>
+            <Nav />
+            {children}
+          </main>
+
+        </Provider>
+
       </body>
     </html>
   )
